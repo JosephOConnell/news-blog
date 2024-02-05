e467fd30ec79f186afbc352e8779601ea495c19c
JosephOConnell <>
2024-02-05 14:39:51 +0000

    LVL27 images

--- forum/views.py
+++ forum/views.py
@@ -9,6 +9,7 @@ class PostList(generic.ListView):
     template_name = "index.html"
     paginate_by = 6
 
+
 class PostDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
@@ -28,4 +29,3 @@ class PostDetail(View):
                 "liked": liked,
             }
         )
-    
